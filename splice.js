bb6664de41c262233366fd00dcca1ba7360cc3d4
Marcos Paulo M Cabral <69735297+MarcosCabral93@users.noreply.github.com>
2021-08-08 08:25:02 -0300

    consertando arquivos

--- splice.js
+++
@@ -1,9 +0,0 @@
-let nomes = ["Lucas", "Rodolfo", "Diogo",
- "Amauri", "Josemar", "MP","Rafael"];
-//1° parametro Posição Inicial/2° Parametro n° de elementos a alterar apos o 1° parametro, 3°parametro o que colocar no lugar 
- nomes.splice(3,2,"Mestre Jedai");
- console.log(nomes)
- nomes.splice(4,0,"FEDERER")
- console.log(nomes)
- nomes.splice(1,nomes.length)
- console.log(nomes)
